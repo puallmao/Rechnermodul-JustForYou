5cafd76159f5e224918676606c5e304fa8863e07
Menzel <menzelli>
2023-01-09 12:04:09 +0100

    test mit button für Kreditberechnung

--- Rechnermodul.Kreditberechnung/src/View/FRM_Kreditberechnung.Designer.cs
+++ Rechnermodul.Kreditberechnung/src/View/FRM_Kreditberechnung.Designer.cs
@@ -29,12 +29,32 @@ namespace Rechnermodul.Kreditberechnung.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_kreditRueckzahlung = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_kreditRueckzahlung
+            // 
+            this.button_kreditRueckzahlung.Location = new System.Drawing.Point(60, 35);
+            this.button_kreditRueckzahlung.Name = "button_kreditRueckzahlung";
+            this.button_kreditRueckzahlung.Size = new System.Drawing.Size(75, 23);
+            this.button_kreditRueckzahlung.TabIndex = 0;
+            this.button_kreditRueckzahlung.Text = "Kredit mit einmaliger Rückzahlung";
+            this.button_kreditRueckzahlung.UseVisualStyleBackColor = true;
+            // 
+            // FRM_Kreditberechnung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_kreditRueckzahlung);
+            this.Name = "FRM_Kreditberechnung";
             this.Text = "Kreditberechnung";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_kreditRueckzahlung;
     }
 }
