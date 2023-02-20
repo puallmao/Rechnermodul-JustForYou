9ff4294498ec11c6a23d9d1f906b216a6483c30c
NYX <vin.breets@gmail.com>
2023-02-20 19:31:14 +0100

    MathematischeFunktionen erstellt

--- Rechnermodul.MathematischeFunktionen/src/View/FRM_MathematischeFunktionen.Designer.cs
+++ Rechnermodul.MathematischeFunktionen/src/View/FRM_MathematischeFunktionen.Designer.cs
@@ -29,12 +29,124 @@ namespace Rechnermodul.MathematischeFunktionen.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.settings_btn = new System.Windows.Forms.Button();
+            this.fak_btn = new System.Windows.Forms.Button();
+            this.input_txtbx = new System.Windows.Forms.TextBox();
+            this.wrz_btn = new System.Windows.Forms.Button();
+            this.enter_btn = new System.Windows.Forms.Button();
+            this.ptz_btn = new System.Windows.Forms.Button();
+            this.prim_btn = new System.Windows.Forms.Button();
+            this.dezi_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // settings_btn
+            // 
+            this.settings_btn.Location = new System.Drawing.Point(14, 14);
+            this.settings_btn.Name = "settings_btn";
+            this.settings_btn.Size = new System.Drawing.Size(87, 23);
+            this.settings_btn.TabIndex = 0;
+            this.settings_btn.Text = "Einstellungen";
+            this.settings_btn.UseVisualStyleBackColor = true;
+            // 
+            // fak_btn
+            // 
+            this.fak_btn.Location = new System.Drawing.Point(13, 100);
+            this.fak_btn.Name = "fak_btn";
+            this.fak_btn.Size = new System.Drawing.Size(89, 51);
+            this.fak_btn.TabIndex = 1;
+            this.fak_btn.Text = "Fakultät";
+            this.fak_btn.UseVisualStyleBackColor = true;
+            this.fak_btn.Click += new System.EventHandler(this.fak_btn_Click);
+            // 
+            // input_txtbx
+            // 
+            this.input_txtbx.Location = new System.Drawing.Point(13, 43);
+            this.input_txtbx.Multiline = true;
+            this.input_txtbx.Name = "input_txtbx";
+            this.input_txtbx.Size = new System.Drawing.Size(184, 51);
+            this.input_txtbx.TabIndex = 2;
+            // 
+            // wrz_btn
+            // 
+            this.wrz_btn.Location = new System.Drawing.Point(108, 100);
+            this.wrz_btn.Name = "wrz_btn";
+            this.wrz_btn.Size = new System.Drawing.Size(89, 51);
+            this.wrz_btn.TabIndex = 3;
+            this.wrz_btn.Text = "Wurzel";
+            this.wrz_btn.UseVisualStyleBackColor = true;
+            this.wrz_btn.Click += new System.EventHandler(this.wrz_btn_Click);
+            // 
+            // enter_btn
+            // 
+            this.enter_btn.Location = new System.Drawing.Point(203, 43);
+            this.enter_btn.Name = "enter_btn";
+            this.enter_btn.Size = new System.Drawing.Size(87, 51);
+            this.enter_btn.TabIndex = 4;
+            this.enter_btn.Text = "Enter";
+            this.enter_btn.UseVisualStyleBackColor = true;
+            this.enter_btn.Click += new System.EventHandler(this.enter_btn_Click);
+            // 
+            // ptz_btn
+            // 
+            this.ptz_btn.Location = new System.Drawing.Point(203, 100);
+            this.ptz_btn.Name = "ptz_btn";
+            this.ptz_btn.Size = new System.Drawing.Size(87, 51);
+            this.ptz_btn.TabIndex = 5;
+            this.ptz_btn.Text = "Potenz";
+            this.ptz_btn.UseVisualStyleBackColor = true;
+            this.ptz_btn.Click += new System.EventHandler(this.ptz_btn_Click);
+            // 
+            // prim_btn
+            // 
+            this.prim_btn.Location = new System.Drawing.Point(14, 157);
+            this.prim_btn.Name = "prim_btn";
+            this.prim_btn.Size = new System.Drawing.Size(87, 51);
+            this.prim_btn.TabIndex = 6;
+            this.prim_btn.Text = "Primzahlen";
+            this.prim_btn.UseVisualStyleBackColor = true;
+            this.prim_btn.Click += new System.EventHandler(this.prim_btn_Click);
+            // 
+            // dezi_btn
+            // 
+            this.dezi_btn.Location = new System.Drawing.Point(108, 157);
+            this.dezi_btn.Name = "dezi_btn";
+            this.dezi_btn.Size = new System.Drawing.Size(182, 51);
+            this.dezi_btn.TabIndex = 7;
+            this.dezi_btn.Text = "Dezimalbruch";
+            this.dezi_btn.UseVisualStyleBackColor = true;
+            this.dezi_btn.Click += new System.EventHandler(this.dezi_btn_Click);
+            // 
+            // FRM_MathematischeFunktionen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(304, 220);
+            this.Controls.Add(this.dezi_btn);
+            this.Controls.Add(this.prim_btn);
+            this.Controls.Add(this.ptz_btn);
+            this.Controls.Add(this.enter_btn);
+            this.Controls.Add(this.wrz_btn);
+            this.Controls.Add(this.input_txtbx);
+            this.Controls.Add(this.fak_btn);
+            this.Controls.Add(this.settings_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FRM_MathematischeFunktionen";
             this.Text = "MathematischeFunktionen";
+            this.Load += new System.EventHandler(this.FRM_MathematischeFunktionen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button settings_btn;
+        private System.Windows.Forms.Button fak_btn;
+        private System.Windows.Forms.TextBox input_txtbx;
+        private System.Windows.Forms.Button wrz_btn;
+        private System.Windows.Forms.Button enter_btn;
+        private System.Windows.Forms.Button ptz_btn;
+        private System.Windows.Forms.Button prim_btn;
+        private System.Windows.Forms.Button dezi_btn;
     }
 }
--- Rechnermodul.MathematischeFunktionen/src/View/FRM_MathematischeFunktionen.cs
+++ Rechnermodul.MathematischeFunktionen/src/View/FRM_MathematischeFunktionen.cs
@@ -12,9 +12,167 @@ namespace Rechnermodul.MathematischeFunktionen.View
 {
     public partial class FRM_MathematischeFunktionen : Form
     {
+        enum Rechenfunktion
+        {
+            Fakultät,
+            Wurzel,
+            Potenz,
+            Primzahlen,
+            Dezimabruch
+        };
+        Rechenfunktion funktion;
+
         public FRM_MathematischeFunktionen()
         {
             InitializeComponent();
+        }
+
+        private void FRM_MathematischeFunktionen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Buttons
+        private void enter_btn_Click(object sender, EventArgs e)
+        {
+            string rechnung = input_txtbx.Text;
+            string ergebnis = "";
+            switch (funktion)
+            {
+                case Rechenfunktion.Fakultät:
+                    fakultätBerechnen(rechnung, ref ergebnis);
+                    input_txtbx.Text = rechnung + " = " + ergebnis;
+                    break;
+                case Rechenfunktion.Wurzel:
+                    wurzelBerechnen(rechnung, ref ergebnis);
+                    input_txtbx.Text = rechnung + " = " + ergebnis;
+                    break;
+                case Rechenfunktion.Potenz:
+                    potenzBerechnen(rechnung, ref ergebnis);
+                    input_txtbx.Text = rechnung + " = " + ergebnis;
+                    break;
+                case Rechenfunktion.Primzahlen:
+                    primzahlenBerechnen(int.Parse(rechnung), ref ergebnis);
+                    input_txtbx.Text = rechnung + " = " + ergebnis;
+                    break;
+                case Rechenfunktion.Dezimabruch:
+                    dezimalbruchBerechnen(rechnung, ref ergebnis);
+                    input_txtbx.Text = rechnung + " = " + ergebnis;
+                    break;
+            }
+        }
+
+        private void fak_btn_Click(object sender, EventArgs e)
+        {
+            input_txtbx.Text = input_txtbx.Text + "!";
+            funktion = Rechenfunktion.Fakultät;
+        }
+
+        private void prim_btn_Click(object sender, EventArgs e)
+        {
+            funktion = Rechenfunktion.Primzahlen;
+        }
+
+        private void wrz_btn_Click(object sender, EventArgs e)
+        {
+            input_txtbx.Text = "√" + input_txtbx.Text;
+            funktion = Rechenfunktion.Wurzel;
+        }
+
+        private void ptz_btn_Click(object sender, EventArgs e)
+        {
+            input_txtbx.Text = input_txtbx.Text + "^";
+            funktion = Rechenfunktion.Potenz;
+        }
+
+        private void dezi_btn_Click(object sender, EventArgs e)
+        {
+            funktion = Rechenfunktion.Dezimabruch;
+        }
+
+
+        // Funktionen
+        public void fakultätBerechnen(string rechung, ref string ergebnis)
+        {
+            //alles außer das Ausrufezeichen
+            int fakultät = int.Parse(rechung.Remove(rechung.Length - 1));
+            int num = 1;
+            for (int i = 1; i <= fakultät; i++)
+            {
+                //num mit sichselbst und i multiplizieren, dadruch ist es um eine Zahl versetzt, bis i gleich der Falkultaet ist
+                num *= i;
+            }
+            ergebnis = num.ToString();
+        }
+
+        public void wurzelBerechnen(string rechung, ref string ergebnis)
+        {
+            int num = int.Parse(rechung.Substring(1));
+            //haelfte von 16 = 8
+            double sqrt = num / 2;
+            double temp = 0;
+            //solange temp != sqrt
+            while (temp != sqrt)
+            {
+                //war 0 jetzt 8
+                //im naechsten durchlauf 5
+                temp = sqrt;
+                //(16 / 8 + 8) / 2 = 5
+                //(16 / 5 + 5) / 2 = 4.1
+                //usw. bis 4 = 4
+                sqrt = (num / temp + temp) / 2;
+            }
+            ergebnis = sqrt.ToString();
+        }
+
+        public void potenzBerechnen(string rechnung, ref string ergebnis)
+        {
+            string[] num = rechnung.Split(new char[] { '^' }, 2, System.StringSplitOptions.RemoveEmptyEntries);
+            int result = 1;
+            int basis = int.Parse(num[0]);
+            int exponent = int.Parse(num[1]);
+            //loop durch Exponent und multipliziere die Basis mit dem Ergebnis aus dem ersten vorherigen loop
+            for (int i = 1; i <= exponent; i++)
+            {
+                result *= basis;
+            }
+            ergebnis = result.ToString();
+        }
+
+        public void primzahlenBerechnen(int num, ref string ergebnis)
+        {
+            List<int> PrimeNumbers = new List<int>();
+            bool Prime = true;
+            //a = 2 weil 1 keine Primzahl ist
+            for (int a = 2; a <= num; a++)
+            {
+                for (int b = 2; b <= num; b++)
+                {
+                    //weil die Zahlen nicht gleich sein druefen und muss beim der Modulo Operation nicht Null sein da Primzahlen nur durch sich und 1 Teilbar sind
+                    if (a != b && a % b == 0)
+                    {
+                        Prime = false;
+                        break;
+                    }
+                }
+                if (Prime)
+                {
+                    //wenn nur durch sich und selbst Teilbar dann Primzahl
+                    PrimeNumbers.Add(a);
+                }
+                Prime = true;
+            }
+            //String aus Liste erstellen und hinzufuegen
+            ergebnis = string.Join(",", PrimeNumbers);
+        }
+
+        public void dezimalbruchBerechnen(string rechnung, ref string ergebnis)
+        {
+            string[] dezimalbruch = rechnung.Split(new char[] {'/'}, 2, StringSplitOptions.RemoveEmptyEntries);
+            double zähler = int.Parse(dezimalbruch[0]);
+            double nenner = int.Parse(dezimalbruch[1]);
+            double potenzwert = zähler / nenner;
+            ergebnis = potenzwert.ToString();
         }
     }
 }
